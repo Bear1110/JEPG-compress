33a07b2d839049a8708d9ae0613937b7fabec389
Bear <william456821@gmail.com>
2018-12-06 19:38:00 +0800

    Update : RGB first version

---
+++ decodeRGB.js
@@ -0,0 +1,269 @@
+fs = require('fs')
+dct = require('./FastDct')
+t = require('./table')
+let data = fs.readFileSync('afterCompress.bear')
+let len = data.length , longSting = ''
+const QF = 50
+for(let i = 0 ; i < len ; i++){    
+    let bin = data[i].toString(2);
+    while(bin.length != 8){
+        bin = '0'+bin
+    }
+    longSting += bin
+}
+//拿掉最後的 10000
+len = longSting.length
+if(longSting.charAt(len-1) == '1'){
+    longSting = longSting.substring(0,--len)
+}else{    
+    do{
+        longSting = longSting.substring(0,--len)
+    }
+    while(longSting.charAt(len-1) != '1')
+}
+let bigPicture = new Array(512)
+for(let i = 0 ; i < 512 ; i++){
+    bigPicture[i] = new Array(512)
+}
+for(let i = 0 ; i < 512 ; i++){
+    for(let j = 0 ; j < 512 ; j++){
+        bigPicture[i][j] = []
+    }
+}
+////////////////////////////////////////////////////////////////////////////先拿y
+len = longSting.length
+let bitI = 0
+let readTemp = ''
+let squares = []
+
+while(squares.length != 4096){
+    readTemp = ''
+    let zigzag = []
+    //dc
+    while(t.decodeDCCategoryCodeWord[readTemp] == undefined){        
+        readTemp += longSting.charAt(bitI++)
+    }
+    let ssss = t.decodeDCCategoryCodeWord[readTemp]
+    readTemp = ''
+    if(ssss ==0){
+        readTemp += longSting.charAt(bitI++)
+    }else{        
+        for(let ggg = 0 ; ggg < ssss; ggg++){
+            readTemp += longSting.charAt(bitI++)
+        }
+    }    
+    zigzag.push(t.decodeDCvalueCodeWord(readTemp)) // first dc
+    while(zigzag.length != 64){//AC
+        readTemp = ''    
+        while(t.decodeACCoefficientsInJPEG[readTemp]  == undefined ){
+            readTemp += longSting.charAt(bitI++)
+        }
+        if(readTemp == '1010'){ // EOB
+            while(zigzag.length != 64){
+                zigzag.push(0)
+            }
+            break
+        }
+        let [run,size] = t.decodeACCoefficientsInJPEG[readTemp]
+        if(run == 15 && size == 0){//ZRL
+            for(let gg = 0 ; gg < 16; gg++)
+                zigzag.push(0)
+        }else{
+            for(let gg = 0 ; gg < run; gg++)
+                zigzag.push(0)
+            readTemp = ''
+            for(let gg = 0 ; gg < size; gg++)
+                readTemp += longSting.charAt(bitI++)
+            zigzag.push(t.decodeDCvalueCodeWord(readTemp))
+        }
+    }
+    squares.push(t.zigzag2square(zigzag))
+}
+
+len = squares.length
+const Luminance = t.LuminanceQF(QF)
+for(let i = 0 ; i < len ; i++){
+    let gg = squares[i]
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = gg[i][j]*Luminance[i][j]
+    gg = dct.idct2d(gg)
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = Math.round(gg[i][j])
+    squares[i] = gg
+}
+for(let i = 0 , j = 0, k = 0; i < len ; i++){//  8*8 4096
+    let square = squares[i]
+    for(let m = 0; m < 8 ; m++){
+        for(let n = 0; n < 8 ; n++){
+            bigPicture[j][k+n][0] = square[m][n]
+        }
+        j++
+    }
+    k+=8
+    if(k == 512){
+        k = 0
+    }else{
+        j -= 8
+    }
+}
+//////////////////////////////////////////////////////////cb
+len = longSting.length
+readTemp = ''
+squares = []
+
+while(squares.length != 4096){
+    readTemp = ''
+    let zigzag = []
+    //dc
+    while(t.decodeDCCategoryCodeWord[readTemp] == undefined){        
+        readTemp += longSting.charAt(bitI++)
+    }
+    let ssss = t.decodeDCCategoryCodeWord[readTemp]
+    readTemp = ''
+    if(ssss ==0){
+        readTemp += longSting.charAt(bitI++)
+    }else{        
+        for(let ggg = 0 ; ggg < ssss; ggg++){
+            readTemp += longSting.charAt(bitI++)
+        }
+    }    
+    zigzag.push(t.decodeDCvalueCodeWord(readTemp)) // first dc
+    while(zigzag.length != 64){//AC
+        readTemp = ''    
+        while(t.decodeACCoefficientsInJPEG[readTemp]  == undefined ){
+            readTemp += longSting.charAt(bitI++)
+        }
+        if(readTemp == '1010'){ // EOB
+            while(zigzag.length != 64){
+                zigzag.push(0)
+            }
+            break
+        }
+        let [run,size] = t.decodeACCoefficientsInJPEG[readTemp]
+        if(run == 15 && size == 0){//ZRL
+            for(let gg = 0 ; gg < 16; gg++)
+                zigzag.push(0)
+        }else{
+            for(let gg = 0 ; gg < run; gg++)
+                zigzag.push(0)
+            readTemp = ''
+            for(let gg = 0 ; gg < size; gg++)
+                readTemp += longSting.charAt(bitI++)
+            zigzag.push(t.decodeDCvalueCodeWord(readTemp))
+        }
+    }
+    squares.push(t.zigzag2square(zigzag))
+}
+
+len = squares.length
+for(let i = 0 ; i < len ; i++){
+    let gg = squares[i]
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = gg[i][j]*Luminance[i][j]
+    gg = dct.idct2d(gg)
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = Math.round(gg[i][j])
+    squares[i] = gg
+}
+for(let i = 0 , j = 0, k = 0; i < len ; i++){//  8*8 4096
+    let square = squares[i]
+    for(let m = 0; m < 8 ; m++){
+        for(let n = 0; n < 8 ; n++){
+            bigPicture[j][k+n][1] = square[m][n]
+        }
+        j++
+    }
+    k+=8
+    if(k == 512){
+        k = 0
+    }else{
+        j -= 8
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////cr
+readTemp = ''
+squares = []
+while(squares.length != 4096){
+    readTemp = ''
+    let zigzag = []
+    //dc
+    while(t.decodeDCCategoryCodeWord[readTemp] == undefined){        
+        readTemp += longSting.charAt(bitI++)
+    }
+    let ssss = t.decodeDCCategoryCodeWord[readTemp]
+    readTemp = ''
+    if(ssss ==0){
+        readTemp += longSting.charAt(bitI++)
+    }else{        
+        for(let ggg = 0 ; ggg < ssss; ggg++){
+            readTemp += longSting.charAt(bitI++)
+        }
+    }    
+    zigzag.push(t.decodeDCvalueCodeWord(readTemp)) // first dc
+    while(zigzag.length != 64){//AC
+        readTemp = ''    
+        while(t.decodeACCoefficientsInJPEG[readTemp]  == undefined ){
+            readTemp += longSting.charAt(bitI++)
+        }
+        if(readTemp == '1010'){ // EOB
+            while(zigzag.length != 64){
+                zigzag.push(0)
+            }
+            break
+        }
+        let [run,size] = t.decodeACCoefficientsInJPEG[readTemp]
+        if(run == 15 && size == 0){//ZRL
+            for(let gg = 0 ; gg < 16; gg++)
+                zigzag.push(0)
+        }else{
+            for(let gg = 0 ; gg < run; gg++)
+                zigzag.push(0)
+            readTemp = ''
+            for(let gg = 0 ; gg < size; gg++)
+                readTemp += longSting.charAt(bitI++)
+            zigzag.push(t.decodeDCvalueCodeWord(readTemp))
+        }
+    }
+    squares.push(t.zigzag2square(zigzag))
+}
+len = squares.length
+for(let i = 0 ; i < len ; i++){
+    let gg = squares[i]
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = gg[i][j]*Luminance[i][j]
+    gg = dct.idct2d(gg)
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = Math.round(gg[i][j])
+    squares[i] = gg
+}
+for(let i = 0 , j = 0, k = 0; i < len ; i++){//  8*8 4096
+    let square = squares[i]
+    for(let m = 0; m < 8 ; m++){
+        for(let n = 0; n < 8 ; n++){
+            bigPicture[j][k+n][2] = square[m][n]
+        }
+        j++
+    }
+    k+=8
+    if(k == 512){
+        k = 0
+    }else{
+        j -= 8
+    }
+}
+
+var buffer = new Uint8Array(512*512*3);
+for(let i = 0,k = 0 ; i < 512 ; i++)
+    for(let j = 0; j < 512 ; j++){
+        buffer[k++] = Math.round(bigPicture[i][j][0] + 1.402*(bigPicture[i][j][2]-128))
+        buffer[k++] = Math.round(bigPicture[i][j][0] - 0.344136*(bigPicture[i][j][1]-128) - 0.714136*(bigPicture[i][j][2]-128))
+        buffer[k++] = Math.round(bigPicture[i][j][0] + 1.772*(bigPicture[i][j][1]-128))
+    }
+fs.writeFileSync('origisn.raw', buffer)
---
+++ encodeRGB.js
@@ -0,0 +1,223 @@
+fs = require('fs');
+dct = require('./FastDct')
+t = require('./table')
+const QF = 50
+let data = fs.readFileSync('img/LenaRGB.raw')
+let long = ''
+let len = data.length
+let array = new Array(262144)
+for(let i = 0 ,g = 0; g < len / 3 ; g++){
+    let R = data[i++]
+    let G = data[i++]
+    let B = data[i++]
+    array[g] = [] //from wiki
+    array[g][0] = 0 + 0.299*R + 0.587*G + 0.114*B //y
+    array[g][1] = 128 - 0.168736*R - 0.331264*G + 0.5*B //cb
+    array[g][2] = 128 + 0.5*R - 0.418688*G - 0.081312*B //cr
+}
+
+///////////////////////////////////////////////////////////////////////先做 y
+len = 262144
+let bigbicture = new Array(512)
+for(let i = 0 ; i < len ; i++){ 
+    let temp =  Math.floor(i/512)
+    if(!bigbicture[temp])
+        bigbicture[temp] = []
+    bigbicture[temp][i%512] = array[i][0]
+}
+len = 4096
+let squares = []
+for(let i = 0 , j = 0, k = 0; i < len ; i++){//  8*8 4096
+    let square = [[],[],[],[],[],[],[],[]]
+    for(let m = 0; m < 8 ; m++){
+        for(let n = 0; n < 8 ; n++){
+            square[m][n] = bigbicture[j][k+n]
+        }
+        j++
+    }
+    squares.push(square)
+    k+=8
+    if(k == 512){
+        k = 0
+    }else{
+        j -= 8
+    }
+}
+
+len = squares.length
+const Luminance = t.LuminanceQF(QF)
+for(let g = 0 ; g < len ; g++){
+    let gg = dct.dct2d(squares[g])    
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = gg[i][j]/Luminance[i][j]
+    let row = t.ZigzagTraversal(gg)
+    row = row.map(e=>Math.round(e))
+    long += t.DCCategoryCodeWord[t.DCCategory(row[0])]+t.DCvalueCodeWord(row[0])
+    let zerocount = 0
+    let allnumber = row.filter(e=>e!=0)
+    for(let i = 1 ,k = 1; i < 64 ; i++){
+        if(row[i] == 0){
+            zerocount++
+            if(zerocount == 16){
+                long += t.ACCoefficientsInJPEG[15][0]
+                zerocount = 0
+            }
+        }else{
+            let ACcodeword = t.DCvalueCodeWord(row[i])
+            long += t.ACCoefficientsInJPEG[zerocount][ACcodeword.length] + ACcodeword
+            zerocount = 0
+            k++
+            if(allnumber.length==k){
+                break
+            }
+        }
+    }
+    long += t.ACCoefficientsInJPEG[0][0] //EOB
+}
+///////////////////////////////////////////////////////////////////////做 cb 4:1:1
+len = 262144
+bigbicture = new Array(512)
+for(let i = 0 ; i < len ; i++){ 
+    let temp =  Math.floor(i/512)
+    if(!bigbicture[temp])
+        bigbicture[temp] = []
+    bigbicture[temp][i%512] = array[i][1] //cb
+}
+len = 4096
+squares = []
+for(let i = 0 , j = 0, k = 0; i < len ; i++){//  8*8 4096
+    let square = [[],[],[],[],[],[],[],[]]
+    for(let m = 0; m < 8 ; m+=2){
+        for(let n = 0; n < 8 ; n+=2){
+            square[m][n] = bigbicture[j][k+n] //左上
+            square[m][n+1] = bigbicture[j][k+n] //右上
+            square[m+1][n] = bigbicture[j][k+n] //左下
+            square[m+1][n+1] = bigbicture[j][k+n] //右下
+        }
+        j+=2
+    }
+    squares.push(square)
+    k+=8
+    if(k == 512){
+        k = 0
+    }else{
+        j -= 8
+    }
+}
+
+len = squares.length
+for(let g = 0 ; g < len ; g++){
+    let gg = dct.dct2d(squares[g])    
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = gg[i][j]/Luminance[i][j]
+    let row = t.ZigzagTraversal(gg)
+    row = row.map(e=>Math.round(e))
+    long += t.DCCategoryCodeWord[t.DCCategory(row[0])]+t.DCvalueCodeWord(row[0])
+    let zerocount = 0
+    let allnumber = row.filter(e=>e!=0)
+    for(let i = 1 ,k = 1; i < 64 ; i++){
+        if(row[i] == 0){
+            zerocount++
+            if(zerocount == 16){
+                long += t.ACCoefficientsInJPEG[15][0]
+                zerocount = 0
+            }
+        }else{
+            let ACcodeword = t.DCvalueCodeWord(row[i])
+            long += t.ACCoefficientsInJPEG[zerocount][ACcodeword.length] + ACcodeword
+            zerocount = 0
+            k++
+            if(allnumber.length==k){
+                break
+            }
+        }
+    }
+    long += t.ACCoefficientsInJPEG[0][0] //EOB
+}
+
+///////////////////////////////////////////////////////////////////////做 cr 4:1:1
+len = 262144
+bigbicture = new Array(512)
+for(let i = 0 ; i < len ; i++){ 
+    let temp =  Math.floor(i/512)
+    if(!bigbicture[temp])
+        bigbicture[temp] = []
+    bigbicture[temp][i%512] = array[i][2] //cb
+}
+len = 4096
+squares = []
+for(let i = 0 , j = 0, k = 0; i < len ; i++){//  8*8 4096
+    let square = [[],[],[],[],[],[],[],[]]
+    for(let m = 0; m < 8 ; m+=2){
+        for(let n = 0; n < 8 ; n+=2){
+            square[m][n] = bigbicture[j][k+n] //左上
+            square[m][n+1] = bigbicture[j][k+n] //右上
+            square[m+1][n] = bigbicture[j][k+n] //左下
+            square[m+1][n+1] = bigbicture[j][k+n] //右下
+        }
+        j+=2
+    }
+    squares.push(square)
+    k+=8
+    if(k == 512){
+        k = 0
+    }else{
+        j -= 8
+    }
+}
+
+len = squares.length
+for(let g = 0 ; g < len ; g++){
+    let gg = dct.dct2d(squares[g])    
+    for(let i = 0 ; i < 8 ; i++)
+        for(let j = 0 ; j < 8 ; j++)
+            gg[i][j] = gg[i][j]/Luminance[i][j]
+    let row = t.ZigzagTraversal(gg)
+    row = row.map(e=>Math.round(e))
+    long += t.DCCategoryCodeWord[t.DCCategory(row[0])]+t.DCvalueCodeWord(row[0])
+    let zerocount = 0
+    let allnumber = row.filter(e=>e!=0)
+    for(let i = 1 ,k = 1; i < 64 ; i++){
+        if(row[i] == 0){
+            zerocount++
+            if(zerocount == 16){
+                long += t.ACCoefficientsInJPEG[15][0]
+                zerocount = 0
+            }
+        }else{
+            let ACcodeword = t.DCvalueCodeWord(row[i])
+            long += t.ACCoefficientsInJPEG[zerocount][ACcodeword.length] + ACcodeword
+            zerocount = 0
+            k++
+            if(allnumber.length==k){
+                break
+            }
+        }
+    }
+    long += t.ACCoefficientsInJPEG[0][0] //EOB
+}
+
+/**
+ * 最後塞 100000 之類的東西
+ * 所以decode的時候應該要先把 結尾 的100000 給刪掉
+ * 如果剛好八的倍數就塞 10000000 這樣
+ */
+long += '1'
+let GG = long.length % 8
+if(GG != 0)
+    for(let KK = 8 - GG ; KK > 0; KK--)
+        long += '0'
+console.log(long.substring(414630))
+var buffer = new Uint8Array(long.length / 8);
+let temp = '', bufferIndex = 0
+for(let i = 0; i < long.length ; i++){
+    let thisBit = long.charAt(i)
+    temp += thisBit
+    if(temp.length == 8){
+        buffer[bufferIndex++] = parseInt(temp, 2)
+        temp = ''
+    }
+}
+fs.writeFileSync('afterCompress.bear', buffer)
